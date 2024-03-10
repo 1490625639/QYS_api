62627c0d2185703a1d1a543b106b23bdad51e33c
张以白 <zhangtongding@foxmail.com>
2024-03-10 17:52:49 +0800

    代码优化

--- testcases/conftest.py
+++ testcases/conftest.py
@@ -12,15 +12,15 @@ import pytest
 from common import Base
 from utils.yaml_util import clear_yaml
 
-"""@pytest.fixture(scope="function", autouse=False,params=['1','2','3'],name='cm')# [['1','2']]执行一次
+"""
+# @pytest.fixture(scope="function", autouse=False)
+@pytest.fixture(scope="function", autouse=False,params=['1','2','3'],name='cm')# [['1','2']]执行一次
 def connect_mysql(request):
     print("链接数据库")
     yield   request.param       #有参数情况下写 多用于多种环境
     print("关闭数据库")"""
 
 @pytest.fixture(scope="session", autouse=False)
-# @pytest.fixture(scope="function", autouse=False)
-
 def connect_mysql():
     clear_yaml()# 会话前清空上一次数据
     print("链接数据库")
